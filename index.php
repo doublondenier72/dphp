e152ff8a98b4db8c5970f7d1557b9e8df9ff9386
Doublondenier72 <ilhann.musi@mail.com>
2022-02-14 20:17:39 +0100

    add bootstrap

--- index.php
+++ index.php
@@ -10,8 +10,10 @@ require_once('php/includes.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DPHP</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.js"></script>
 </body>
 </html>
